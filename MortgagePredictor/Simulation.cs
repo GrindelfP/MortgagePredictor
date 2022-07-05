da6395386313b026cd40f74dd28423c36d4388c1
Grindelf <grindelf.perlomutrovij@gmail.com>
2022-07-05 16:34:28 +0300

    add FreeMoney, Mortgage, Simulation, TestSubject classes

--- MortgagePredictor/Simulation.cs
+++ MortgagePredictor/Simulation.cs
@@ -1,7 +1,15 @@
-﻿namespace MortgagePredictor
+﻿using System.Collections.Generic;
+
+namespace MortgagePredictor
 {
     internal class Simulation
     {
+        private List<TestSubject> testSubjects;
 
+        public Simulation(double apartmentCost, double rentAmount, FreeMoney freeMoney, Mortgage mortgage, BankDeposit bankDeposit)
+        {
+            testSubjects = new List<TestSubject>();
+            for (var i = 0; i < 11; i++) testSubjects.Add(new TestSubject(apartmentCost, rentAmount, freeMoney, mortgage, bankDeposit));
+        }
     }
 }
---
+++ MortgagePredictor/TestSubject.cs
@@ -0,0 +1,20 @@
+﻿namespace MortgagePredictor
+{
+    internal class TestSubject
+    {
+        public double ApartmentCost { get; }
+        public double RentAmount { get; }
+        public FreeMoney FreeMoney { get; }
+        public Mortgage Mortgage { get; }
+        public BankDeposit BankDeposit { get; } 
+
+        public TestSubject(double apartmentCost, double rentAmount, FreeMoney freeMoney, Mortgage mortgage, BankDeposit bankDeposit)
+        {
+            ApartmentCost = apartmentCost;
+            RentAmount = rentAmount;
+            FreeMoney = freeMoney;
+            Mortgage = mortgage;
+            BankDeposit = bankDeposit;
+        }
+    }
+}
