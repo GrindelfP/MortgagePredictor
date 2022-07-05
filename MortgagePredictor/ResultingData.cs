e3717b19de25458740b5e6078509a19ae213068a
Grindelf <63808275+GrindelfP@users.noreply.github.com>
2022-07-05 19:54:10 +0300

    Create ResultingData.cs

---
+++ MortgagePredictor/ResultingData.cs
@@ -0,0 +1,8 @@
+namespace MortgagePredictor
+{
+    internal struct ResultData
+    {
+        internal double DebetAcountAmount { get; set; }
+        internal int MonthNumber { get; set; }
+    }
+}
